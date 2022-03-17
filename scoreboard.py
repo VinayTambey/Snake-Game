37581be0c763545170321255d64f73f7a410ce64
Vinay Tambey <vinaytambey000@gmail.com>
2022-03-17 14:29:21 +0530

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -4,7 +4,10 @@ import random
 
 ALIGN = "center"
 FONT = ("Arial", 24, "normal")
+
+
 class Scoreboard(Turtle):
+
     def __init__(self):
         super().__init__()
         self.color("white")
@@ -12,18 +15,21 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.goto(0, 260)
         self.score = 0
+        self.high_score = 0
         self.write(f"Score:{self.score}", align=ALIGN, font=FONT)
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score:{self.score}", align=ALIGN, font=FONT)
+        self.clear()
+        self.write(f"Score:{self.score}  High Score: {self.high_score}", align=ALIGN, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("Game Over!", align=ALIGN, font=FONT)
 
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
